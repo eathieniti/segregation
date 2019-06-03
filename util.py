e95695d25df211545895c32b7361837e12374e52
eathieniti <efi.athieniti@gmail.com>
2019-06-03 11:59:16 +0200

    fixed bug in segregation measure for fully segregated neighbouhoods

--- util.py
+++ util.py
@@ -1,8 +1,17 @@
 import numpy as np
 import sys
 
+"""
+Utilities file, includes helper functions and measures
+"""
+
 def segregation_index(model, unit = "school" ):
 
+    """
+    Calculates the local compositions for schools/neighbourhoods and then the segregation
+    @unit: "school", "neighbourhood" or "agents_neighbourhood"
+
+    """
     pi_jm = np.zeros(shape=(len(model.school_locations), len(model.household_types)))
     local_compositions = np.zeros(shape=(len(model.school_locations), len(model.household_types)))
 
@@ -43,7 +52,10 @@ def segregation_index(model, unit = "school" ):
 
 
     seg_index = calculate_segregation_index(local_compositions,pi_jm)
-    return(seg_index)
+    if seg_index:
+        return(seg_index)
+    else:
+        return(np.nan)
 
 
 def calculate_segregation_index(local_compositions, pi_jm):
@@ -64,7 +76,9 @@ def calculate_segregation_index(local_compositions, pi_jm):
 
     tj = np.sum(local_compositions,axis=1, keepdims=True)
 
-    pm = np.sum(pi_jm,axis=0)/np.sum(pi_jm, keepdims=True)
+    #pm = np.sum(pi_jm,axis=0)/np.sum(pi_jm, keepdims=True)
+    pm = np.sum(local_compositions,axis=0)/np.sum(local_compositions, keepdims=True)
+
     #print("pm",pm)
 
     E = np.sum(pm*np.log(1/pm))
@@ -72,12 +86,22 @@ def calculate_segregation_index(local_compositions, pi_jm):
     #print("tj/TE",tj / (T * E))
     #print("pi_jm",pi_jm)
     #print("pm",pm)
-    #print("pi_jm/pm",pi_jm/pm)
 
-    seg_index = np.sum(tj / (T*E) * pi_jm * np.ma.log(pi_jm/pm), axis=None)
+    log_matrix= np.nan_to_num(np.log(pi_jm/pm))
+    print('log',log_matrix)
 
+    seg_index = np.sum(tj / (T*E) * pi_jm * log_matrix, axis=None)
 
+    print("pm", pm)
 
+    if seg_index>1:
+        print("tj",tj )
+
+        print("tj/TE",tj / (T * E))
+        print("pi_jm",pi_jm)
+        print("pi_jm/pm",pi_jm/pm)
+
+        print("pm",pm)
 
     return(seg_index)
 

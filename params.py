3049bac26ae7353bbcbe40e65d1472d2c2073538
efi <efi@dta170056.science.uva.nl>
2019-09-18 17:12:45 +0200

    new parameter settings

--- params.py
+++ params.py
@@ -1,7 +1,7 @@
 density = 0.9; num_schools = 64; minority_pc =  0.50; f0 =  0.70; f1 =  0.70; M0 =  0.8; M1 =  0.8;
-alpha =  0.8; temp =  0.1; cap_max =2; move = "boltzmann"; symmetric_positions = True;
-schelling=True;bounded=True;radius=6;T=0.95;
-school_moves_per_step=2000; residential_moves_per_step=2000;fs="eq";variable_f=True;sigma=0.5;sample=1;
+alpha =  0.2; temp =  0.1; cap_max =2; move = "boltzmann"; symmetric_positions = True;
+schelling=False;bounded=True;radius=3;T=0.75;
+school_moves_per_step=2000; residential_moves_per_step=2000;fs="eq";variable_f=True;sigma=0.1;sample=1;
 num_neighbourhoods=16; schools_per_neighbourhood=1;displacement=1;
 i=0
 
